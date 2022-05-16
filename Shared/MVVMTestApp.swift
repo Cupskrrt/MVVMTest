ef53c25b1448c88916339f9a8c9174683eabebc5
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-16 22:28:43 +0700

    Initial Commit

---
+++ Shared/MVVMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTestApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
